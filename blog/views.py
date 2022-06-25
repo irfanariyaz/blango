850d56b504a99c42a7d9707d8629782d299b1414
irfana Sulaiman <irfanakumble@gmail.com>
2022-06-25 19:42:27 +0000

    Finish html Framework

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.conf import settings
 
 # Create your views here.
+def index(request):
+    print(settings.BASE_DIR)
+    return render(request, "blog/index.html")
